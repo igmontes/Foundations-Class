f8bbab32f52126413cc77d1677885b2c080bab71
igmontes <ignacio.montes@ymail.com>
2016-07-12 00:53:38 -0700

    Updated version

--- Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Program.cs
@@ -19,45 +19,54 @@ namespace Homework3
             int hour = 3600;
             int day = 86400;
 
-            // ask user for a number of seconds
+            do
+            {
             StartProgram:
-            Console.WriteLine("Enter how many seconds you want ");
-            seconds = Console.ReadLine();
+                // ask user for a number of seconds
+                Console.WriteLine("Enter how many seconds you want, or enter 'end' to quit ");
+                seconds = Console.ReadLine();
+                if (seconds == "end")
+                {
+                    Console.WriteLine("Thanks for playing along.");
+                    Console.ReadLine();
+                    return;
 
-            // ask user what to convert the seconds to
-            Console.WriteLine("Convert to minutes, hours, or days? ");
-            convert = Console.ReadLine();
+                }
 
-            // verify we are converting to a valid selection
-            switch (convert)
-            {
-                case "minutes":
-                    finalvalue = Convert.ToDecimal(seconds) / minute;
-                    break;
-
-                case "hours":
-                    finalvalue = Convert.ToDecimal(seconds) / hour;
-                    break;
-
-                default:
-                    finalvalue = Convert.ToDecimal(seconds) / day;
-                    break;
-            }
-
-            // display the output
-            /* if (convert != "minutes" || convert != "hours" || convert != "days")
-            {
-                Console.WriteLine("Please enter a valid value to convert to");
-                Console.WriteLine("Press enter and start again.");
+                // ask user what to convert the seconds to
+                Console.WriteLine("Convert to minutes, hours, or days? ");
                 convert = Console.ReadLine();
-                goto StartProgram;
-            } 
-            else
-            { */
+
+                // verify we are converting to a valid selection
+                switch (convert)
+                {
+                    case "minutes":
+                        finalvalue = Convert.ToDecimal(seconds) / minute;
+                        break;
+
+                    case "hours":
+                        finalvalue = Convert.ToDecimal(seconds) / hour;
+                        break;
+
+                    case "days":
+                        finalvalue = Convert.ToDecimal(seconds) / day;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a valid value to convert to");
+                        Console.WriteLine("Press enter and start again.");
+                        convert = Console.ReadLine();
+                        goto StartProgram;
+                        break;
+                }
+
+                // display the output
                 Console.WriteLine("You have chosen to convert to {0}", convert);
                 Console.WriteLine("Your final value is {0} {1}", finalvalue, convert);
                 Console.ReadLine();
-            /* } */
+
+            } while (seconds != "end");
+
         }
     }
 }
