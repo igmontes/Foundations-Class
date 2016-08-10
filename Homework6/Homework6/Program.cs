189f022387dfc157d5a4cd5b61b6bfa8073e2542
igmontes <ignacio.montes@ymail.com>
2016-08-09 17:28:17 -0700

    Met the assignment only, need to enhance.

---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    class Television
+    {
+        private int channel = 0;
+        private int volume = 0;
+        private bool isOn = false;
+
+
+        public int Channel
+        {
+            get
+            {
+                return channel;
+            }
+            set
+            {
+                channel = value;
+            }
+        }
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                volume = value;
+            }
+        }
+
+        public bool IsOn
+        {
+            get
+            {
+                return isOn;
+            }
+            set
+            {
+                isOn = value;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Television tv = new Television();
+
+            if (!tv.IsOn)
+            {
+                tv.IsOn = true;
+            }
+
+            tv.Channel = 3;
+
+            tv.Volume++;
+            tv.Volume++;
+            tv.Volume++;
+            tv.Volume++;
+
+            tv.IsOn = false;
+        }
+    }
+}
